106c85ce0b1e571813ede265337ff1056bc3f71f
pix <pixqc1159@gmail.com>
2024-09-08 03:18:13 +0000

    wip actually check impl against jax

--- tensor.py
+++ tensor.py
@@ -12,14 +12,15 @@ class Tensor:
     self.grad = None
     self._ctx: Optional[Function] = None
 
+  @property
+  def shape(self):
+    return self.data.shape
+
   def relu(self):
     return Relu.apply(self)
 
   def tanh(self):
     return Tanh.apply(self)
-
-  def logsoftmax(self, axis=None):
-    return LogSoftmax.apply(self, axis)
 
   def add(self, other):
     return Add.apply(self, other)
@@ -66,10 +67,12 @@ class Relu(Function):
 
 class Tanh(Function):
   def forward(self, x):
-    return Tensor(np.tanh(x))
+    res = Tensor(np.tanh(x))
+    self.res = res
+    return res
 
   def backward(self, out_grad):
-    self.prev[0].grad = out_grad * (1 - self.prev[0].data ** 2)
+    self.prev[0].grad = out_grad * (1 - self.res.data**2)
 
 
 class Add(Function):
@@ -77,8 +80,15 @@ class Add(Function):
     return Tensor(x + y)
 
   def backward(self, out_grad):
-    self.prev[0].grad = out_grad
-    self.prev[1].grad = out_grad
+    if self.prev[0].shape != out_grad.shape:
+      self.prev[0].grad = np.sum(out_grad, axis=0, keepdims=True)
+    else:
+      self.prev[0].grad = out_grad
+
+    if self.prev[1].shape != out_grad.shape:
+      self.prev[1].grad = np.sum(out_grad, axis=0, keepdims=True)
+    else:
+      self.prev[1].grad = out_grad
 
 
 class Sum(Function):
