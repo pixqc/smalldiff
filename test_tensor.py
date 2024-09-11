d20e73941c909df17236307f706a100fd81e6300
pix <pixqc1159@gmail.com>
2024-09-11 13:37:31 +0000

    use decorator to skip some test

--- test_tensor.py
+++ test_tensor.py
@@ -24,6 +24,7 @@ class TestGrad(unittest.TestCase):
     assert isinstance(x.grad, Tensor)
     assert isinstance(x_tiny.grad, TinyTensor)
     self.assertTrue(x.grad.shape == x_tiny.grad.shape)
+    self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
     self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
 
   def test_unary(self):
@@ -54,6 +55,7 @@ class TestGrad(unittest.TestCase):
     assert isinstance(y_tiny.grad, TinyTensor)
     self.assertTrue(x.grad.shape == x_tiny.grad.shape)
     self.assertTrue(y.grad.shape == y_tiny.grad.shape)
+    self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
     self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
     self.assertTrue(np.allclose(y.grad.numpy(), y_tiny.grad.numpy()))
 
@@ -95,6 +97,7 @@ class TestGrad(unittest.TestCase):
     assert isinstance(y_tiny.grad, TinyTensor)
     self.assertTrue(x.grad.shape == x_tiny.grad.shape)
     self.assertTrue(y.grad.shape == y_tiny.grad.shape)
+    self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
     self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
     self.assertTrue(np.allclose(y.grad.numpy(), y_tiny.grad.numpy()))
 
@@ -123,6 +126,7 @@ class TestGrad(unittest.TestCase):
     assert isinstance(x.grad, Tensor)
     assert isinstance(x_tiny.grad, TinyTensor)
     self.assertTrue(x.grad.shape == x_tiny.grad.shape)
+    self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
     self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
 
   def test_reduce(self):
@@ -132,53 +136,113 @@ class TestGrad(unittest.TestCase):
         for axis in range(len(shape)):
           self._test_reduce(func_name, shape, axis=axis)
 
-  # def test_mlp_mnist(self):
-  #   x_np = np.random.randn(784).astype(np.float32)
-  #   y_np = np.random.randint(10, size=10).astype(np.int32)
-  #   w1_np = np.random.randn(784, 256).astype(np.float32)
-  #   b1_np = np.random.randn(256).astype(np.float32)
-  #   w2_np = np.random.randn(256, 10).astype(np.float32)
-  #   b2_np = np.random.randn(10).astype(np.float32)
-  #
-  #   x = Tensor(x_np)
-  #   y = Tensor(y_np)
-  #   w1 = Tensor(w1_np, requires_grad=True)
-  #   b1 = Tensor(b1_np, requires_grad=True)
-  #   w2 = Tensor(w2_np, requires_grad=True)
-  #   b2 = Tensor(b2_np, requires_grad=True)
-  #   out = x.matmul(w1).add(b1).matmul(w2).add(b2)
-  #   out = out.sparse_categorical_crossentropy(y)
-  #   print(out)
-  #   out.backward()
-  #
-  #   x_tiny = TinyTensor(x_np)
-  #   y_tiny = TinyTensor(y_np)
-  #   w1_tiny = TinyTensor(w1_np)
-  #   b1_tiny = TinyTensor(b1_np, requires_grad=True)
-  #   w2_tiny = TinyTensor(w2_np, requires_grad=True)
-  #   b2_tiny = TinyTensor(b2_np, requires_grad=True)
-  #   out_tiny = x_tiny.matmul(w1_tiny).add(b1_tiny).matmul(w2_tiny).add(b2_tiny)
-  #   out_tiny = out_tiny.sparse_categorical_crossentropy(y_tiny)
-  #   out_tiny.backward()
-  #
-  #   assert isinstance(x.grad, Tensor)
-  #   assert isinstance(y.grad, Tensor)
-  #   assert isinstance(w1.grad, Tensor)
-  #   assert isinstance(b1.grad, Tensor)
-  #   assert isinstance(w2.grad, Tensor)
-  #   assert isinstance(b2.grad, Tensor)
-  #   assert isinstance(x_tiny.grad, TinyTensor)
-  #   assert isinstance(y_tiny.grad, TinyTensor)
-  #   assert isinstance(w1_tiny.grad, TinyTensor)
-  #   assert isinstance(b1_tiny.grad, TinyTensor)
-  #   assert isinstance(w2_tiny.grad, TinyTensor)
-  #   assert isinstance(b2_tiny.grad, TinyTensor)
-  #   self.assertTrue(np.allclose(x.grad.data, x_tiny.grad.numpy()))
-  #   self.assertTrue(np.allclose(y.grad.data, y_tiny.grad.numpy()))
-  #   self.assertTrue(np.allclose(w1.grad.data, w1_tiny.grad.numpy()))
-  #   self.assertTrue(np.allclose(b1.grad.data, b1_tiny.grad.numpy()))
-  #   self.assertTrue(np.allclose(w2.grad.data, w2_tiny.grad.numpy()))
-  #   self.assertTrue(np.allclose(b2.grad.data, b2_tiny.grad.numpy()))
+  def test_softmax(self):
+    x_np = np.random.randn(3).astype(np.float32)
+
+    x = Tensor(x_np, requires_grad=True)
+    out = x.softmax()
+    print(out.numpy())
+    out = out.sum()
+    out.backward()
+
+    x_tiny = TinyTensor(x_np, requires_grad=True)
+    out_tiny = x_tiny.softmax()
+    print(out_tiny.numpy())
+    out_tiny = out_tiny.sum()
+    out_tiny.backward()
+
+    print("--")
+    print(x.grad.numpy())
+    print(x_tiny.grad.numpy())
+
+    assert isinstance(x.grad, Tensor)
+    assert isinstance(x_tiny.grad, TinyTensor)
+    self.assertTrue(x.grad.shape == x_tiny.grad.shape)
+    self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
+    self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
+
+  @unittest.skip("not supported yet")
+  def test_log_softmax(self):
+    x_np = np.random.randn(10, 3).astype(np.float32)
+
+    x = Tensor(x_np)
+    out = x.log_softmax()
+    out = out.sum()
+    out.backward()
+
+    x_tiny = TinyTensor(x_np)
+    out_tiny = x_tiny.log_softmax()
+    out_tiny = out_tiny.sum()
+    out_tiny.backward()
+
+    assert isinstance(x.grad, Tensor)
+    assert isinstance(x_tiny.grad, TinyTensor)
+    self.assertTrue(x.grad.shape == x_tiny.grad.shape)
+    self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
+
+  @unittest.skip("not supported yet")
+  def test_crossentropy(self):
+    x_np = np.random.randn(3, 10).astype(np.float32)
+    y_np = np.random.randint(10, size=3).astype(np.int32)
+
+    x = Tensor(x_np)
+    y = Tensor(y_np)
+    y = np.eye(10)[y.data]  # one hot
+
+    out = x.softmax().log().mul(y).mean()
+    # .neg().div(Tensor(3))
+    print(out)
+
+    x_tiny = TinyTensor(x_np)
+    y_tiny = TinyTensor(y_np)
+    out_tiny = x_tiny.sparse_categorical_crossentropy(y_tiny)
+
+    print(out.numpy())
+    print(out_tiny.numpy())
+
+  @unittest.skip("not supported yet")
+  def test_mlp_mnist(self):
+    x_np = np.random.randn(10, 784).astype(np.float32)
+    y_np = np.random.randint(10, size=10).astype(np.int32)
+    w1_np = np.random.randn(784, 256).astype(np.float32)
+    b1_np = np.random.randn(256).astype(np.float32)
+    w2_np = np.random.randn(256, 10).astype(np.float32)
+    b2_np = np.random.randn(10).astype(np.float32)
+
+    x = Tensor(x_np)
+    y = Tensor(y_np)
+    w1 = Tensor(w1_np, requires_grad=True)
+    b1 = Tensor(b1_np, requires_grad=True)
+    w2 = Tensor(w2_np, requires_grad=True)
+    b2 = Tensor(b2_np, requires_grad=True)
+    out = x.matmul(w1).add(b1).tanh().matmul(w2).add(b2)
+    out = out.sparse_categorical_crossentropy(y)
+    out.backward()
+
+    x_tiny = TinyTensor(x_np)
+    y_tiny = TinyTensor(y_np)
+    w1_tiny = TinyTensor(w1_np, requires_grad=True)
+    b1_tiny = TinyTensor(b1_np, requires_grad=True)
+    w2_tiny = TinyTensor(w2_np, requires_grad=True)
+    b2_tiny = TinyTensor(b2_np, requires_grad=True)
+    out_tiny = x_tiny.matmul(w1_tiny).add(b1_tiny).tanh()
+    out_tiny = out_tiny.matmul(w2_tiny).add(b2_tiny)
+    out_tiny = out_tiny.sparse_categorical_crossentropy(y_tiny)
+    out_tiny.backward()
+
+    self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
+    assert isinstance(w1.grad, Tensor)
+    assert isinstance(b1.grad, Tensor)
+    assert isinstance(w2.grad, Tensor)
+    assert isinstance(b2.grad, Tensor)
+    assert isinstance(w1_tiny.grad, TinyTensor)
+    assert isinstance(b1_tiny.grad, TinyTensor)
+    assert isinstance(w2_tiny.grad, TinyTensor)
+    assert isinstance(b2_tiny.grad, TinyTensor)
+    self.assertTrue(np.allclose(w1.grad.numpy(), w1_tiny.grad.numpy()))
+    self.assertTrue(np.allclose(b1.grad.numpy(), b1_tiny.grad.numpy()))
+    self.assertTrue(np.allclose(w2.grad.numpy(), w2_tiny.grad.numpy()))
+    self.assertTrue(np.allclose(b2.grad.numpy(), b2_tiny.grad.numpy()))
 
 
 if __name__ == "__main__":
