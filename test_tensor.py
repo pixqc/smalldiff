03fbacd298fc9fe3f458233afd472e3bca9705cd
pix <pixqc1159@gmail.com>
2024-09-12 05:52:27 +0000

    remove softmax test, add composite_00 test

--- test_tensor.py
+++ test_tensor.py
@@ -130,119 +130,39 @@ class TestGrad(unittest.TestCase):
     self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
 
   def test_reduce(self):
-    for func_name in ["max", "sum", "mean"]:
+    for func_name in ["max", "sum", "mean"]:  # TODO: prod
       for shape in [(3,), (3, 4), (3, 4, 5)]:
         self._test_reduce(func_name, shape, axis=None)
         for axis in range(len(shape)):
           self._test_reduce(func_name, shape, axis=axis)
 
-  def test_softmax(self):
-    x_np = np.random.randn(3).astype(np.float32)
+  def test_composite_00(self):
+    x_np = np.random.randn(2, 4).astype(np.float32)
+    w_np = np.random.randn(4, 5).astype(np.float32)
+    b_np = np.random.randn(5).astype(np.float32)
 
     x = Tensor(x_np, requires_grad=True)
-    out = x.softmax()
-    print(out.numpy())
-    out = out.sum()
+    w = Tensor(w_np, requires_grad=True)
+    b = Tensor(b_np, requires_grad=True)
+    out = x.matmul(w).add(b).tanh().sum()
     out.backward()
 
     x_tiny = TinyTensor(x_np, requires_grad=True)
-    out_tiny = x_tiny.softmax()
-    print(out_tiny.numpy())
-    out_tiny = out_tiny.sum()
-    out_tiny.backward()
-
-    print("--")
-    print(x.grad.numpy())
-    print(x_tiny.grad.numpy())
-
-    assert isinstance(x.grad, Tensor)
-    assert isinstance(x_tiny.grad, TinyTensor)
-    self.assertTrue(x.grad.shape == x_tiny.grad.shape)
-    self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
-    self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
-
-  @unittest.skip("not supported yet")
-  def test_log_softmax(self):
-    x_np = np.random.randn(10, 3).astype(np.float32)
-
-    x = Tensor(x_np)
-    out = x.log_softmax()
-    out = out.sum()
-    out.backward()
-
-    x_tiny = TinyTensor(x_np)
-    out_tiny = x_tiny.log_softmax()
-    out_tiny = out_tiny.sum()
-    out_tiny.backward()
-
-    assert isinstance(x.grad, Tensor)
-    assert isinstance(x_tiny.grad, TinyTensor)
-    self.assertTrue(x.grad.shape == x_tiny.grad.shape)
-    self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
-
-  @unittest.skip("not supported yet")
-  def test_crossentropy(self):
-    x_np = np.random.randn(3, 10).astype(np.float32)
-    y_np = np.random.randint(10, size=3).astype(np.int32)
-
-    x = Tensor(x_np)
-    y = Tensor(y_np)
-    y = np.eye(10)[y.data]  # one hot
-
-    out = x.softmax().log().mul(y).mean()
-    # .neg().div(Tensor(3))
-    print(out)
-
-    x_tiny = TinyTensor(x_np)
-    y_tiny = TinyTensor(y_np)
-    out_tiny = x_tiny.sparse_categorical_crossentropy(y_tiny)
-
-    print(out.numpy())
-    print(out_tiny.numpy())
-
-  @unittest.skip("not supported yet")
-  def test_mlp_mnist(self):
-    x_np = np.random.randn(10, 784).astype(np.float32)
-    y_np = np.random.randint(10, size=10).astype(np.int32)
-    w1_np = np.random.randn(784, 256).astype(np.float32)
-    b1_np = np.random.randn(256).astype(np.float32)
-    w2_np = np.random.randn(256, 10).astype(np.float32)
-    b2_np = np.random.randn(10).astype(np.float32)
-
-    x = Tensor(x_np)
-    y = Tensor(y_np)
-    w1 = Tensor(w1_np, requires_grad=True)
-    b1 = Tensor(b1_np, requires_grad=True)
-    w2 = Tensor(w2_np, requires_grad=True)
-    b2 = Tensor(b2_np, requires_grad=True)
-    out = x.matmul(w1).add(b1).tanh().matmul(w2).add(b2)
-    out = out.sparse_categorical_crossentropy(y)
-    out.backward()
-
-    x_tiny = TinyTensor(x_np)
-    y_tiny = TinyTensor(y_np)
-    w1_tiny = TinyTensor(w1_np, requires_grad=True)
-    b1_tiny = TinyTensor(b1_np, requires_grad=True)
-    w2_tiny = TinyTensor(w2_np, requires_grad=True)
-    b2_tiny = TinyTensor(b2_np, requires_grad=True)
-    out_tiny = x_tiny.matmul(w1_tiny).add(b1_tiny).tanh()
-    out_tiny = out_tiny.matmul(w2_tiny).add(b2_tiny)
-    out_tiny = out_tiny.sparse_categorical_crossentropy(y_tiny)
+    w_tiny = TinyTensor(w_np, requires_grad=True)
+    b_tiny = TinyTensor(b_np, requires_grad=True)
+    out_tiny = x_tiny.matmul(w_tiny).add(b_tiny).tanh().sum()
     out_tiny.backward()
 
     self.assertTrue(np.allclose(out.numpy(), out_tiny.numpy()))
-    assert isinstance(w1.grad, Tensor)
-    assert isinstance(b1.grad, Tensor)
-    assert isinstance(w2.grad, Tensor)
-    assert isinstance(b2.grad, Tensor)
-    assert isinstance(w1_tiny.grad, TinyTensor)
-    assert isinstance(b1_tiny.grad, TinyTensor)
-    assert isinstance(w2_tiny.grad, TinyTensor)
-    assert isinstance(b2_tiny.grad, TinyTensor)
-    self.assertTrue(np.allclose(w1.grad.numpy(), w1_tiny.grad.numpy()))
-    self.assertTrue(np.allclose(b1.grad.numpy(), b1_tiny.grad.numpy()))
-    self.assertTrue(np.allclose(w2.grad.numpy(), w2_tiny.grad.numpy()))
-    self.assertTrue(np.allclose(b2.grad.numpy(), b2_tiny.grad.numpy()))
+    assert isinstance(x.grad, Tensor)
+    assert isinstance(w.grad, Tensor)
+    assert isinstance(b.grad, Tensor)
+    assert isinstance(x_tiny.grad, TinyTensor)
+    assert isinstance(w_tiny.grad, TinyTensor)
+    assert isinstance(b_tiny.grad, TinyTensor)
+    self.assertTrue(np.allclose(x.grad.numpy(), x_tiny.grad.numpy()))
+    self.assertTrue(np.allclose(w.grad.numpy(), w_tiny.grad.numpy()))
+    self.assertTrue(np.allclose(b.grad.numpy(), b_tiny.grad.numpy()))
 
 
 if __name__ == "__main__":
